b640ff095df5b57d9458d8b17d1615758d45afd7
Michelitaaa0 <michelasantilli.13@gmail.com>
2025-12-12 01:07:23 +0100

    agregar estructura de header y secciones de inicio y lugares en HTML; implementar menú responsive en JavaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* ===========================
+   MENÚ RESPONSIVE
+=========================== */
+
+function openMenu() {
+    document.querySelector(".menu-items").style.left = "0";
+}
+
+function closeMenu() {
+    document.querySelector(".menu-items").style.left = "-100%";
+}

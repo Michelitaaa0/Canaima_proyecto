affe49e046f23726aa72e49da6c4c80c2512c3dc
Michelitaaa0 <michelasantilli.13@gmail.com>
2025-12-12 01:09:27 +0100

    agregar estilos para el texto del héroe, secciones y lugares; implementar modal y galería en HTML y JavaScript

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ function openMenu() {
 function closeMenu() {
     document.querySelector(".menu-items").style.left = "-100%";
 }
+
+/* ===========================
+   OPACIDAD DEL MENÚ AL HACER SCROLL
+=========================== */
+
+window.onscroll = function () {
+    let scroll = window.scrollY;
+    let menu = document.getElementById("menu");
+
+    if (scroll > 50) {
+        menu.style.backgroundColor = "rgba(0,0,0,0.6)";
+    } else {
+        menu.style.backgroundColor = "transparent";
+    }
+};
+document.getElementById("modal").addEventListener("click", function(e) {
+    if (e.target === this) {
+        closeModal();
+    }
+});
